708252fcd5ba7e4c9060c6181a4e6072231fe59c
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-03 13:40:50 +0000

    - checkin method to hotel class

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -2,28 +2,42 @@ import java.util.ArrayList;
 
 public class Hotel {
     private Guest guest;
-    private ArrayList<Bedroom> bedrooms;
-    private ArrayList<DiningRoom> diningrooms;
-    private ArrayList<ConferenceRoom> conferenceRooms;
+    private Bedroom bedroom;
+    private ArrayList<Bedroom> availableBedrooms;
+//    private ArrayList<DiningRoom> diningrooms;
+//    private ArrayList<ConferenceRoom> conferenceRooms;
 
-    public Hotel(Guest guest){
+    public Hotel(Guest guest, Bedroom bedroom, ArrayList<Bedroom> availableBedrooms) {
         this.guest = guest;
-        this.bedrooms = new ArrayList<Bedroom>();
-        this.diningrooms = new ArrayList<DiningRoom>();
-        this.conferenceRooms = new ArrayList<ConferenceRoom>();
+        this.bedroom = bedroom;
+        this.availableBedrooms = availableBedrooms;
+//        this.diningrooms = new ArrayList<DiningRoom>();
+//        this.conferenceRooms = new ArrayList<ConferenceRoom>();
+//    }
+
+//
+//
+//    }
     }
 
-    public void checkAvailableRooms(){
+    public int availableBedRooms(){
+       return availableBedrooms.size();
 
     }
+
+    public int checkin(){
+        //add guest to room
+
+        // once added, remove that room from available room array
+        if (bedroom.getGuests().size() == 0);
+        bedroom.addGuest(guest);
+        this.availableBedrooms.remove(0);
+        return availableBedrooms.size();
+
+        //
+    }
+
+
 }
 
 
-//- (ArrayList Rooms) or do the below arrays
-//        - ArrayList Bedrooms -> so can check guests in
-//        - ArrayList Dining rooms -> so can add guests just here
-//        - in charge of checking guests in and out, but method would be in room (add guest)
-//        - Room.addguest(guest)  —> from Room
-//
-//        use for loop to check available rooms
-//        e.g.  for room in rooms, if room. is available add to rooms
