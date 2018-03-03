8a2e8e1efe169cdfa12114ecdced934474bb2713
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-03 18:20:55 +0000

    - further methods added to hotel

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -4,20 +4,21 @@ public class Hotel {
     private Guest guest;
     private Bedroom bedroom;
     private ArrayList<Bedroom> availableBedrooms;
-//    private ArrayList<DiningRoom> diningrooms;
+    private DiningRoom diningRoom;
+    private ArrayList<DiningRoom> diningrooms;
 //    private ArrayList<ConferenceRoom> conferenceRooms;
 
-    public Hotel(Guest guest, Bedroom bedroom, ArrayList<Bedroom> availableBedrooms) {
+    public Hotel(Guest guest, Bedroom bedroom, ArrayList<Bedroom> availableBedrooms, DiningRoom diningRoom, ArrayList<DiningRoom> diningRooms) {
+
         this.guest = guest;
         this.bedroom = bedroom;
         this.availableBedrooms = availableBedrooms;
-//        this.diningrooms = new ArrayList<DiningRoom>();
+        this.diningRoom = diningRoom;
+        this.diningrooms = new ArrayList<DiningRoom>();
 //        this.conferenceRooms = new ArrayList<ConferenceRoom>();
 //    }
 
-//
-//
-//    }
+
     }
 
     public int availableBedRooms(){
@@ -27,14 +28,22 @@ public class Hotel {
 
     public int checkin(){
         //add guest to room
-
         // once added, remove that room from available room array
         if (bedroom.getGuests().size() == 0);
         bedroom.addGuest(guest);
         this.availableBedrooms.remove(0);
         return availableBedrooms.size();
+    }
 
-        //
+    public void addGuestToDiningRoom(){
+        diningRoom.addGuest(guest);
+
+    }
+
+    public String findGuestNameinRoom(){
+        bedroom.addGuest(guest);
+        return guest.getName();
+
     }
 
 
